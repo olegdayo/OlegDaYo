f4bf3a723eb989b97121d0468990e8ebbdd4d246
olegdayo <ol-sidorenkov@mail.ru>
2025-12-30 22:05:31 +0300

    ci

---
+++ scripts/hbd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+line=$(less README.md | grep 'y. o.')
+
+age=0
+
+for word in $(echo $line | cut -d ' ' -f 1-); do
+    # Checks if word is an integer.
+    if [ "$word" -eq "$word" 2> /dev/null ]; then
+        age=$word
+    fi
+done
+
+echo "$age"
+
+sed -i "s/I'm a .* y. o./I'm a $((age+1)) y. o./g" ./README.md
